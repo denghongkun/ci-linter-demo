c206d03e8074c60f21a7f7e2881b8c7b80a04e5f
denghongkun1 <1395978447@qq.com>
2025-10-12 21:02:40 +0800

    test: add bad code to test CI failure

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ def calculate_total(x, y, z):
 if __name__ == "__main__":
     calculate_sum(5, 10)
     calculate_total(1, 2, 3)
+
+# 在文件末尾添加不规范代码
+def bad_function  ():
+    x=1
+    print("This line is way too long and should trigger flake8 error because it exceeds the maximum line length limit of 79 characters")
